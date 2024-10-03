9e5470bf75dcf19bbe1116c7a45475d36ac223fd
fhrrldla <hrg6450@naver.com>
2024-10-03 14:59:54 +0900

    [Bronze III] Title: 약수 구하기, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/2501. 약수 구하기/약수 구하기.c
@@ -0,0 +1,25 @@
+
+#include <stdio.h>
+
+int main(void)
+{
+    int n, k;
+    int tmp = 0;
+
+    scanf("%d %d", &n, &k);
+
+    for(int i = 1; i <= n; i++)
+    {
+        if(n % i == 0)
+        {   tmp ++;
+            if(tmp == k)
+            {
+                printf("%d", i);
+                break;
+            }
+        }
+    }
+    if(tmp < k)
+        printf("0\n");
+    return 0;
+}
