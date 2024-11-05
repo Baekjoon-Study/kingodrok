71aceb76acc53b53121d9beb5ef32aa742e054fd
fhrrldla <hrg6450@naver.com>
2024-11-05 17:03:40 +0900

    [Bronze I] Title: 소인수분해, Time: 24 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/11653. 소인수분해/소인수분해.c
+++ 백준/Bronze/11653. 소인수분해/소인수분해.c
@@ -1,19 +1,4 @@
 #include <stdio.h>
-
 int main(void){
-    int n, i = 2;
-    scanf("%d", &n);
-
-    while(n > 1){
-        if(n % i == 0){
-            n /= i;
-            printf("%d ", i);
-            i = 2;
-        }
-        else{
-            i++;
-        }
-    }
-
-    return 0;
-}
+    int n, i = 2;scanf("%d", &n);
+    while(n>1){n%i==0?n/=i,printf("%d ",i),i=2:i++;}}
